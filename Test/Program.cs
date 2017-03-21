da3b2ce91c88f586a2947785a11541b07219495d
Sam <ArcticEcho@users.noreply.github.com>
2017-03-21 02:48:17 +0000

    magic

--- Test/Program.cs
+++ Test/Program.cs
@@ -15,69 +15,56 @@ namespace Test
         static void Main(string[] args)
         {
             var client = new DiscordClient();
-            client.MessageReceived += (o, e) =>
+            client.MessageReceived += async (o, e) =>
             {
                 if (e.Message.Text == ">VC_TEST" && e.User.VoiceChannel != null)
                 {
-                    Task.Run(async () =>
+                    var ffmpegPsi = new ProcessStartInfo
                     {
-                        var ffmpegPsi = new ProcessStartInfo
-                        {
-                            FileName = Config.Ffmpeg,
-                            Arguments = "-i song.mp4 -f s16le -ar 48000 -ac 2 pipe:1",
-                            UseShellExecute = false,
-                            RedirectStandardOutput = true
-                        };
-                        var framesPerSec = 50;
-                        var targetBufferSecs = 5;
-                        var targetBufferFrames = framesPerSec * targetBufferSecs;
-                        var avgBytesPerSec = 48000 * 2 * 2;
-                        var frameSize = avgBytesPerSec / framesPerSec;
-                        var audioService = client.GetService<AudioService>();
+                        FileName = Config.Ffmpeg,
+                        Arguments = "-i song.mp4 -f s16le -ar 48000 -ac 2 pipe:1",
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true
+                    };
+                    var framesPerSec = 50;
+                    var targetBufferSecs = 5;
+                    var targetBufferFrames = framesPerSec * targetBufferSecs;
+                    var avgBytesPerSec = 48000 * 2 * 2;
+                    var frameSize = avgBytesPerSec / framesPerSec;
+                    var audioService = client.GetService<AudioService>();
+                    var audioClient = await audioService.Join(e.User.VoiceChannel).ConfigureAwait(true);
 
-                        Console.WriteLine("1");
-                        var audioClient = await audioService.Join(e.User.VoiceChannel);
-                        Console.WriteLine("2");
-
-                        var bufferedFrames = new Queue<byte[]>();
-                        var fileFullyRead = false;
-                        var ffmpegMre = new ManualResetEvent(false);
-                        Task.Run(() =>
+                    var bufferedFrames = new Queue<byte[]>();
+                    var fileFullyRead = false;
+                    var ffmpegMre = new ManualResetEvent(false);
+                    Task.Run(() =>
+                    {
+                        var ffmpegProc = Process.Start(ffmpegPsi);
+                        while (!fileFullyRead)
                         {
-                            var ffmpegProc = Process.Start(ffmpegPsi);
-                            while (!fileFullyRead)
+                            while (bufferedFrames.Count > targetBufferFrames)
                             {
-                                while (bufferedFrames.Count > targetBufferFrames)
-                                {
-                                    Thread.Sleep(5);
-                                }
-                                var b = new byte[frameSize];
-                                var byteCount = ffmpegProc.StandardOutput.BaseStream.Read(b, 0, b.Length);
-                                if (byteCount == 0)
-                                    fileFullyRead = true;
-                                bufferedFrames.Enqueue(b);
-                                Thread.Sleep(15);
+                                Thread.Sleep(5);
                             }
-                            ffmpegProc.Close();
-                            ffmpegMre.Set();
-                        });
-                        while (bufferedFrames.Count < targetBufferFrames)
-                        {
-                            Thread.Sleep(100);
+                            var b = new byte[frameSize];
+                            var byteCount = ffmpegProc.StandardOutput.BaseStream.Read(b, 0, b.Length);
+                            if (byteCount == 0)
+                                fileFullyRead = true;
+                            bufferedFrames.Enqueue(b);
+                            Thread.Sleep(15);
                         }
-                        while (bufferedFrames.Count > 0)
-                        {
-                            try
-                            {
-                                var b = bufferedFrames.Dequeue();
-                                audioClient.Send(b, 0, b.Length);
-                            }
-                            catch (Exception ex)
-                            {
-                                Console.WriteLine(ex);
-                            }
-                        }
+                        ffmpegProc.Close();
+                        ffmpegMre.Set();
                     });
+                    while (bufferedFrames.Count < targetBufferFrames)
+                    {
+                        Thread.Sleep(100);
+                    }
+                    while (bufferedFrames.Count > 0)
+                    {
+                        var b = bufferedFrames.Dequeue();
+                        audioClient.Send(b, 0, b.Length);
+                    }
                 }
             };
             client.UsingAudio(x => x.Mode = AudioMode.Outgoing);
